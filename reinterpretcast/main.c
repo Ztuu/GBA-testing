8717907d5364239925a88404372a44eb0528585b
Stu <stuartpdev@gmail.com>
2020-05-12 09:32:01 +0100

    Tonc and reinterpretcast "hello world" examples

--- reinterpretcast/main.c
+++ reinterpretcast/main.c
@@ -7,14 +7,17 @@ int main(void)
 	volatile unsigned char *ioram = (unsigned char *)0x04000000;
 	ioram[0] = 0x03; // Use video mode 3 (in BG2, a 16bpp bitmap in VRAM)
 	ioram[1] = 0x04; // Enable BG2 (BG0 = 1, BG1 = 2, BG2 = 4, ...)
-
 	// Write pixel colours into VRAM
 	vram = (unsigned short *)0x06000000;
-	// vram[80*240 + 115] = 0x001F; // X = 115, Y = 80, C = 000000000011111 = R
+
+
+	//Draw a line
 	int i;
-	for (i=0; i< 100; i++){
-		draw_pixel(115+i,90, 0x001F);
+	for (i=0; i< 240; i++){
+		draw_pixel(i, 0, 0x001F);
 	}
+
+	//Draw some dots
 	draw_pixel(115, 80, 0x001F);
 	vram[80*240 + 120] = 0x03E0; // X = 120, Y = 80, C = 000001111100000 = G
 	vram[80*240 + 125] = 0x7C00; // X = 125, Y = 80, C = 111110000000000 = B
